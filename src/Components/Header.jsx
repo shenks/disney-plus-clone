7cd5ebbbcacb7c55188788314e8a561f4daaf4be
shenkslee <shenkslee@gmail.com>
2024-04-15 19:49:06 +0800

    header UI change for diff sizes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -43,9 +43,16 @@ function Header() {
     <div className="flex items-center justify-between p-5">
       <div className="flex gap-8 items-center">
         <img src={logo} className="w-[80px] md:w-[115px]" />
-        {menu.map((item) => (
-          <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
-        ))}
+        <div className="hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+        <div className="md:hidden flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
+          ))}
+        </div>
       </div>
       <img src={profile} className="w-[40px]" />
     </div>
