defc98e2a4ea7630cdef8f9f3e85038436082b84
shenkslee <shenkslee@gmail.com>
2024-04-15 21:41:38 +0800

    fix branch errors

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -50,7 +50,6 @@ function Header() {
           ))}
         </div>
         <div className="md:hidden flex gap-8">
-
           {menu.map(
             (item, index) =>
               index < 3 && (
@@ -60,9 +59,6 @@ function Header() {
           <div className="md:hidden">
             <HeaderItem name={""} Icon={HiDotsVertical} />
           </div>
-          {menu.map((item) => (
-            <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
-          ))}
         </div>
       </div>
       <img src={profile} className="w-[40px] rounded-full" />
