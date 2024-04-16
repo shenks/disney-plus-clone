3adc25fdb701998ecbd8f546f47ec1e201573b9f
shenkslee <shenkslee@gmail.com>
2024-04-16 15:05:58 +0800

    api setup

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+const movieBaseUrl = "https://api.themoviedb.org/3";
+const api_key = "28ea26224d25c751b7b51e73dd27292d";
+const getTrendingVideos = axios.get(
+  this.movieBaseUrl + "/trending/all/day?api_key" + api_key
+);
+
+export default {
+  getTrendingVideos,
+};
