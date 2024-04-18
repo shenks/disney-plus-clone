d1c1409cff2a1a0ff1ecd62fa738897f6fa71503
shenkslee <shenkslee@gmail.com>
2024-04-18 23:25:03 +0800

    production house component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Slider from "./Components/Slider";
+import ProductionHouse from "./Components/ProductionHouse";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Slider />
+      <ProductionHouse />
     </>
   );
 }
