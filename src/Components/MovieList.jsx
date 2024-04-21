ccc2e670a24ebca8eebe0cea1bc89f1248ee36db
shenkslee <shenkslee@gmail.com>
2024-04-21 16:02:06 +0800

    MovieList maps to MovieCard

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,18 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GlobalApi from "../Services/GlobalApi";
+import MovieCard from "./MovieCard";
 
 function MovieList({ genreId }) {
+  const [movieList, setMovieList] = useState([]);
   useEffect(() => {
     getMovieByGenreId();
   });
 
   const getMovieByGenreId = () => {
     GlobalApi.getMovieByGenreId(genreId).then((resp) => {
-      console.log(resp.data.results);
+      setMovieList(resp.data.results);
     });
   };
 
-  return <div>movies</div>;
+  return (
+    <div>
+      {movieList.map((item, index) => (
+        <MovieCard />
+      ))}
+    </div>
+  );
 }
 
 export default MovieList;
