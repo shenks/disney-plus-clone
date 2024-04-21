3aa218a1ad4c61231849a6a4f7e52fb7bb0632ab
shenkslee <shenkslee@gmail.com>
2024-04-21 16:09:50 +0800

    MovieCard appears

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
-function MovieCard() {
-  return <div>movie</div>;
+function MovieCard({ movie }) {
+  return (
+    <div>
+      <img src={IMAGE_BASE_URL + movie.poster_path} className="w-[110px]" />
+    </div>
+  );
 }
 
 export default MovieCard;
